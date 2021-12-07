2540ff499dddbd84ec82cc156e9b547455493956
Marcin Janus <janekmachine@gmail.com>
2021-12-07 10:49:39 +0100

    Parcel initial

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Zadania rekrutacyjne
+
+[Zadanie 1](https://schriker.github.io/grupa_it_tasks/Task_1/)
+
+[Zadanie 2](https://schriker.github.io/grupa_it_tasks/Task_2/)
+
+[Zadanie 3](https://schriker.github.io/grupa_it_tasks/Task_3/)
+
+[Zadanie 4](https://schriker.github.io/grupa_it_tasks/Task_4/)
--- Task_1/app.js
+++ Task_1/app.js
@@ -1,28 +1,33 @@
 const oddColumn = document.querySelector('.odd');
 const evenColumn = document.querySelector('.even');
 
-function render(numbers, container) {
-  container.innerHTML = '';
+function render(numbers) {
+  oddColumn.innerHTML = '';
+  evenColumn.innerHTML = '';
 
-  numbers.sort(function (a, b) {
-    return a - b;
-  }).forEach(function (number) {
-    container.innerHTML = container.innerHTML + `<div>${number}</div>`;
+  numbers.forEach(function (number) {
+    if (number % 2 === 0) {
+      evenColumn.innerHTML = evenColumn.innerHTML + `<div>${number}</div>`;
+    } else {
+      oddColumn.innerHTML = oddColumn.innerHTML + `<div>${number}</div>`;
+    }
   });
 }
 
 document.querySelector('button').addEventListener('click', function () {
-  const odd = [];
-  const even = [];
+  const numbers = [];
 
-  while (odd.length + even.length < 20) {
+  while (numbers.length < 20) {
     const number = Math.floor(Math.random() * 100) + 1;
 
-    if (!odd.includes(number) && !even.includes(number)) {
-      number % 2 === 0 ? even.push(number) : odd.push(number);
+    if (!numbers.includes(number)) {
+      numbers.push(number);
     }
   }
 
-  render(odd, oddColumn);
-  render(even, evenColumn);
+  numbers.sort(function (a, b) {
+    return a - b;
+  });
+
+  render(numbers);
 });
